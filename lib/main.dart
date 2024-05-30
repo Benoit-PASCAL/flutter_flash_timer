64bc39b41a4f18f775b4be9ec0da10f1f489b9f6
Benoît PASCAL <pcl.ben.6@gmail.com>
2024-05-30 09:02:26 +0200

    feat: add buttons to set timer

--- lib/main.dart
+++ lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:async';
 import 'dart:math';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
+import 'package:flutter/widgets.dart';
 import 'package:wakelock/wakelock.dart';
 
 // my phone screen : 922 x 414
@@ -25,6 +27,7 @@ class MyApp extends StatelessWidget {
             foregroundColor: MaterialStateProperty.all<Color>(const Color.fromRGBO(186, 186, 186, 1)),
             textStyle: MaterialStateProperty.all<TextStyle>(
               const TextStyle(
+                color: Colors.grey
                 // fontFamily: 'inherit',
               )
             )
@@ -53,8 +56,10 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   double bg_opacity = 0.0;
   bool increaseOpacity = true;
-  static const maxTime = 60 * 20;
-  int second = maxTime;
+  int maxTime = 60 * 20;
+  int second = 0;
+  int base = 1;
+  bool modifySec = false;
   bool isStarted = false;
   bool isRunning = false;
   bool isFinished = false;
@@ -63,10 +68,29 @@ class _MyHomePageState extends State<MyHomePage> {
   IconData icon = Icons.play_arrow;
   DateTime? end;
 
+  ScrollController minController = ScrollController();
+
   @override
   void initState() {
     Wakelock.enable();
     super.initState();
+
+    second = maxTime;
+    minController.addListener(_scrollDown);
+  }
+
+  void _scrollDown() {
+    // if(minController.position.) {
+    //   increaseTime();
+    // }
+  }
+
+  @override
+  void dispose() {
+    // TODO: implement dispose
+    Wakelock.disable();
+    minController.removeListener(_scrollDown);
+    super.dispose();
   }
 
   void handlerButtonPressed() {
@@ -161,6 +185,38 @@ class _MyHomePageState extends State<MyHomePage> {
     flashTimer?.cancel();
   }
 
+  void setBase(base) {
+    setState(() {
+      this.base = base;
+    });
+  }
+
+  void setModifySec(modifySec) {
+    setState(() {
+      this.modifySec = modifySec;
+    });
+  }
+
+  void decreaseTime() {
+    print('decreased');
+    if(!isRunning) {
+      setState(() {
+        maxTime = max(maxTime - (base * (modifySec ? 1 : 60)), 0);
+        second = max(second - (base * (modifySec ? 1 : 60)), 0);
+      });
+    }
+  }
+
+  void increaseTime() {
+    print('increased');
+    if(!isRunning) {
+      setState(() {
+        maxTime = maxTime + (base * (modifySec ? 1 : 60));
+        second = second + (base * (modifySec ? 1 : 60));
+      });
+    }
+  }
+  
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -174,50 +230,150 @@ class _MyHomePageState extends State<MyHomePage> {
                 color: Colors.white.withOpacity(bg_opacity),
               ),
             ),
-            Container(
-              alignment: Alignment.center,
-              child: Column(
-                mainAxisAlignment: MainAxisAlignment.center,
-                children: [
-                  const Padding(
-                    padding: EdgeInsets.only(top: 120.0),
-                    child: Text(
-                      'Flash Timer',
-                      style: TextStyle(
-                        fontSize: 40,
-                        color: Colors.grey,
+            Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Container(
+                  height: MediaQuery.of(context).size.height * 0.8,
+                  alignment: Alignment.center,
+                  child: Column(
+                    mainAxisAlignment: MainAxisAlignment.center,
+                    children: [
+                      const Padding(
+                        padding: EdgeInsets.only(top: 180.0),
+                        child: Text(
+                          'Flash Timer',
+                          style: TextStyle(
+                            fontSize: 40,
+                            color: Colors.grey,
+                          ),
+                        ),
                       ),
-                    ),
-                  ),
-                  Text(
-                    '${(second/60).truncate()} m ${(second%60).round()}',
-                    style: const TextStyle(
-                      fontSize: 80,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  Text(
-                    '${end ?? ''}',
-                    style: const TextStyle(
-                      fontSize: 15,
-                      color: Colors.grey,
-                    ),
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.only(top: 100.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        handlerButtonPressed();
+                      Container(
+                        height: 100,
+                        child: Text(
+                          '${(second/60).truncate()} m ${(second%60).round()}',
+                          style: const TextStyle(
+                            fontSize: 80,
+                            color: Colors.grey,
+                          ),
+                        ),
+                      ),
+                      Text(
+                        '${end ?? ''}',
+                        style: const TextStyle(
+                          fontSize: 15,
+                          color: Colors.grey,
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.only(top: 100.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                          children: [
+                            ElevatedButton(
+                              onPressed: () {
+                                decreaseTime();
+                              },
+                              child: const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Icon(Icons.remove,
+                                    size: 20),
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                handlerButtonPressed();
+                              },
+                              child: Padding(
+                                padding: const EdgeInsets.all(18.0),
+                                child: Icon(icon,
+                                  size: 40),
+                              ),
+                            ),
+                            ElevatedButton(
+                              onPressed: () {
+                                increaseTime();
+                              },
+                              child: const Padding(
+                                padding: EdgeInsets.all(8.0),
+                                child: Icon(Icons.add,
+                                    size: 20),
+                              ),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => {
+                        setModifySec(true)
                       },
-                      child: Padding(
-                        padding: const EdgeInsets.all(18.0),
-                        child: Icon(icon,
-                          size: 40),
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(modifySec ? Colors.black : Color.fromRGBO(55, 55, 55, 1)),
                       ),
-                    ),
-                  ),
-                ],
-              )
+                      child: Text('sec')),
+                    ElevatedButton(
+                      onPressed: () => {
+                        setModifySec(false)
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(!modifySec ? Colors.black : Color.fromRGBO(55, 55, 55, 1)),
+                      ),
+                      child: Text('min')),
+                  ],
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: [
+                    ElevatedButton(
+                      onPressed: () => {
+                        setBase(1)
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(base == 1 ? Colors.black : Color.fromRGBO(55, 55, 55, 1)),
+                      ),
+                      child: Text('1')),
+                    ElevatedButton(
+                      onPressed: () => {
+                        setBase(2)
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(base == 2 ? Colors.black : Color.fromRGBO(55, 55, 55, 1)),
+                      ),
+                      child: Text('2')),
+                    ElevatedButton(
+                      onPressed: () => {
+                        setBase(5)
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(base == 5 ? Colors.black : Color.fromRGBO(55, 55, 55, 1)),
+                      ),
+                      child: Text('5')),
+                    ElevatedButton(
+                      onPressed: () => {
+                        setBase(10)
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(base == 10 ? Colors.black : Color.fromRGBO(55, 55, 55, 1)),
+                      ),
+                      child: Text('10')),
+                    ElevatedButton(
+                      onPressed: () => {
+                        setBase(15)
+                      },
+                      style: ButtonStyle(
+                        backgroundColor: MaterialStateProperty.all<Color>(base == 15 ? Colors.black : Color.fromRGBO(55, 55, 55, 1)),
+                      ),
+                      child: Text('15')),
+                  ],
+                )
+              ],
             )
           ],
         ),
